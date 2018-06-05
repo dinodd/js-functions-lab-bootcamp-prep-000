8efa7a476d32d43541e49a60cb7ecfbf656ab708
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-05 01:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function happyHolidaysTo(name) {
   return `Happy hoolidays, ${name}!`
 }
 happyHolidaysTo('Dino')
+
+function happyHolidaysTo(holiday,name) {
+  return `Happy ${holiday}, ${name}`
+}
+happyHolidaysTo('Birthday','Dino')
+
+function holidayCountdown(days,holiday) {
+  `It's ${days} days until ${holiday}`
+}
+holidayCountdown('4',"'Dino's birthday")
